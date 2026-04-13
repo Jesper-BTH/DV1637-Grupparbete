2abba2b6760514601937ca086ed39ab90dc97f6a
Gurkanan <mattis.andersson@live.se>
2026-04-13 13:26:14 +0200

    Worked on coffin script

--- Assets/Scripts/Mattis/Coffin.cs
+++ Assets/Scripts/Mattis/Coffin.cs
@@ -1,10 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Coffin : MonoBehaviour
 {
+    int PointsHit;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        PointsHit = 0;
         
     }
 
@@ -16,6 +19,22 @@ public class Coffin : MonoBehaviour
 
     public void WeakPointHit(GameObject piece)
     {
+        PointsHit =+ 1;
+        piece.transform.tag = "Broken";
+        piece.transform.Translate(0,0.00001f,0);
+        
+        if(PointsHit >= 3)
+        {
+            int kids = gameObject.transform.childCount;
+            for (int i = 0; i < kids; i++)
+            {
+                gameObject.transform.GetChild(i).AddComponent<Rigidbody>();
+                gameObject.transform.GetChild(i).AddComponent<SphereCollider>();
+
+                gameObject.transform.GetChild(i).GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-2, 2), 2,Random.Range(-2, 2)), ForceMode.Impulse);
+            }//Scatter pieces
+            gameObject.transform.GetComponent<BoxCollider>().enabled = false;
+        }//breaks the object into pieces when 3 weakpoints have been hit.
 
     }
 }
