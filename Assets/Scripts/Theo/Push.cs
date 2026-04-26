79f027a41271627120fa5ccffa63f7de03e5171e
Gurkanan <mattis.andersson@live.se>
2026-04-26 19:03:23 +0200

    basic GraveStone, fence, delta time to push

--- Assets/Scripts/Theo/Push.cs
+++ Assets/Scripts/Theo/Push.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Push : MonoBehaviour
 {
-    public float pushStrength = 2f;
+    public float pushStrength = 150f;
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
@@ -13,6 +13,6 @@ public class Push : MonoBehaviour
 
         Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
 
-        rb.AddForce(pushDir * pushStrength, ForceMode.Impulse);
+        rb.AddForce(pushDir * pushStrength * Time.deltaTime, ForceMode.Impulse); // added delta time -Mattis
     }
 }
