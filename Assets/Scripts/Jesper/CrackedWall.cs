002579a210eec2ed2fbcaa136e252f7e39843185
Jesper-BTH <jeol25@student.bth.se>
2026-05-06 12:59:19 +0200

    stuffing bin doing

---
+++ Assets/Scripts/Jesper/CrackedWall.cs
@@ -0,0 +1,47 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CrackedWall : MonoBehaviour
+{
+    int PointsHit;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PointsHit = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void WeakPointHit(Collider piece)
+    {
+        PointsHit++;
+        piece.transform.tag = "Block";
+        piece.transform.Translate(0, 1f, 0);//Feedback on weakhit
+        Debug.Log("Pointhit: " + PointsHit);
+
+        if (PointsHit >= 3)
+        {
+            int kids = gameObject.transform.childCount;
+            for (int i = 0; i < kids; i++)
+            {
+                Debug.Log(gameObject.transform.GetChild(i));
+                gameObject.transform.GetChild(i).AddComponent<Rigidbody>();
+                gameObject.transform.GetChild(i).GetComponent<Rigidbody>().mass = 3;
+                gameObject.transform.GetChild(i).GetComponent<MeshCollider>().enabled = false;
+                gameObject.transform.GetChild(i).AddComponent<BoxCollider>();
+                //gameObject.transform.GetChild(i).GetComponent<BoxCollider>().size = new Vector3(gameObject.transform.GetChild(i).GetComponent<BoxCollider>().size.x, gameObject.transform.GetChild(i).GetComponent<BoxCollider>().size.y * 6, gameObject.transform.GetChild(i).GetComponent<BoxCollider>().size.z);
+
+                gameObject.transform.GetChild(i).GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-0.5f, 0.5f), 0.5f, Random.Range(-0.5f, 0.5f)), ForceMode.Impulse);
+            }//Scatter pieces
+            gameObject.transform.GetComponent<BoxCollider>().enabled = false;
+            Destroy(gameObject, 2f);
+        }//breaks the object into pieces when 3 weakpoints have been hit.
+
+    }
+}
