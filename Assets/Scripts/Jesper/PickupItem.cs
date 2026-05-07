2729a7b43a21608031ad90408db97ea64f61286a
Gurkanan <mattis.andersson@live.se>
2026-05-07 13:39:58 +0200

    Gräjer

--- Assets/Scripts/Jesper/PickupItem.cs
+++ Assets/Scripts/Jesper/PickupItem.cs
@@ -8,6 +8,11 @@ public class PickupItem : MonoBehaviour, IInteractable
     {
         if (type != InteractionType.Interact) return;
 
+        /*if (itemType == ItemType.Matches)
+        {
+            GameObject.Find("Player").transform.GetChild(1).GetComponent<Matches_Timer>().addMatch(gameObject.GetComponent<MatchBox>().Amount);
+        }//Special case when you pick up a matchbox - Mattis*/
+
         player.inventory.AddItem(itemType);
         Destroy(gameObject);
 
--- Assets/Scripts/Mattis/MatchBox.cs
+++ Assets/Scripts/Mattis/MatchBox.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class MatchBox : MonoBehaviour
 {
+    public int Amount;
+    GameObject match;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        match = gameObject.transform.GetChild(0).gameObject;
+        for (int i = 1; i < Amount; i++)
+        {
+            Instantiate(match, new Vector3(match.transform.position.x + (0.002f*i), match.transform.position.y, match.transform.position.z), Quaternion.identity, gameObject.transform);
+        }
     }
 
     // Update is called once per frame
